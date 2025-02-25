1746d7c83c16242fc8997c66587e78bf035d12e0
shabaraba <work.shabaraba@gmail.com>
2025-02-26 00:27:32 +0900

    feat: mod setup.sh for link

--- setup.sh
+++ setup.sh
@@ -44,4 +44,11 @@ find "$PWD"/Indicators -mindepth 1 -maxdepth 1 -type d | while IFS= read -r dir;
     echo "🚚 Created symlink: $dir -> $BASE_DIR/Indicators/$dir_name"
 done
 
+echo "🚚 link here to mql directory..."
+escaped_src=$PWD
+escaped_dest=$(echo "$BASE_DIR" | sed 's/ /\\ /g')
+eval "ln -snvf $escaped_src $escaped_dest"
+eval "ln -snvf $escaped_dest $escaped_src"
+echo "🚚 Created symlink: $escaped_src -> $BASE_DIR"
+
 echo "🎉 deploy finished."
